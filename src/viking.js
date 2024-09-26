70af2be7d46eac2a3c051e6c723375b6821b77e7
Juan Manuel <j-grafi@hotmail.com>
2024-09-26 17:44:04 +0200

    90% completed, no bonus 5

--- src/viking.js
+++ src/viking.js
@@ -31,7 +31,7 @@ class Viking extends Soldier{
        
     }
     battleCry(){
-        return `¡Odin Owns You All!`
+        return "Odin Owns You All!";
 
     }
 }
@@ -51,4 +51,48 @@ class Saxon extends Soldier {
 }
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+addViking(viking){
+    this.vikingArmy.push(viking)
+}
+addSaxon(saxon){
+    this.saxonArmy.push(saxon)
+}
+vikingAttack(){
+    const randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+    const randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+    const result = randomSaxon.receiveDamage(randomViking.strength);
+
+
+    if (randomSaxon.health <= 0) {
+        this.saxonArmy.splice(randomSaxon)
+    }
+    return result;
+}
+saxonAttack(){
+    const randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+    const randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+    const result = randomViking.receiveDamage(randomSaxon.strength);
+    if (randomViking.health <= 0) {
+    this.vikingArmy.splice(randomViking)
+    }
+    return result;
+}
+showStatus(){
+    if (this.saxonArmy.length === 0) {
+        return "Vikings have won the war of the century!"
+    }
+    else if(this.vikingArmy.length===0){
+        return "Saxons have fought for their lives and survived another day..."
+    }
+    else if(this.saxonArmy.length > 0 && this.vikingArmy.length > 0){
+        return "Vikings and Saxons are still in the thick of battle."
+    }      
+}
+
+
+}
